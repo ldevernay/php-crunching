e480ec82ffac01a086d4d7adc2bf33dd45fc39ba
ldevernay <boblehamster@yahoo.fr>
2017-04-05 16:02:11 +0200

    total top10cheapest

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-
+// La somme des prix des dix premiers
 $string = file_get_contents("films.json", FILE_USE_INCLUDE_PATH);
 $brut = json_decode($string, true);
 $top = $brut["feed"]["entry"]; # liste de films
@@ -12,6 +12,28 @@ foreach ($top10 as $key => $film) {
   $total_price += $film["im:price"]["attributes"]["amount"];
 }
 
-echo $total_price;
+// echo $total_price;
+
+
+// Les dix moins chers du top 100
+function compare($a,$b){
+  if ($a["im:price"]["attributes"]["amount"] == $b["im:price"]["attributes"]["amount"]){
+    return 0;
+  }
+
+  return ($a["im:price"]["attributes"]["amount"] < $b["im:price"]["attributes"]["amount"])?-1:1;
+}
+
+usort($top, 'compare');
+
+// foreach ($top as $key => $film) {
+//   echo $film["im:name"]["label"]." : ".$film["im:price"]["attributes"]["amount"]."<br/>";
+// }
+
+$top10cheapest = array_slice($top, 0, 9);
+
+foreach ($top10cheapest as $key => $film) {
+  echo $film["im:name"]["label"]." : ".$film["im:price"]["attributes"]["amount"]."<br/>";
+}
 
  ?>
